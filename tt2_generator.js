bca2f3fa1aadad35e426c0d0f5c506f279f96389
Flare576 <flare576@gmail.com>
2020-11-10 23:04:56 -0600

    gud

--- tt2_generator.js
+++ tt2_generator.js
@@ -1,30 +1,106 @@
 /*
-
-Rules: X/Y coords MUST have 2 decimal places
+Rules: X/Y coords MUST have at least 1 decimal place
 MouseDown and first MouseMove must NOT have same values on a drag
 */
+/* Equipment drop bounding box:
+63.91, 43.98   87.02, 44.3
+  _________________
+  |               |
+  |               |
+  |_______________|
+66.4, 53.69    87.02,54.57
+*/
+const fs = require('fs');
+const allSkills = {
+    activate: {
+        HS: 8.09,
+        DS: 25.01,
+        HoM: 41.47,
+        FS: 58.09,
+        WC: 75.01,
+        SC: 91.32,
+        Y: 89.55,
+    },
+    levelUp: {
+        HS: 39.33,
+        DS: 48.33,
+        HoM: 57.33,
+        FS: 66.33,
+        WC: 75.33,
+        SC: 84.35,
+        oneX: 83.55,
+        maxX: 58.93,
+    },
+};
+const astralLocations = [
+    { X: 3.69, Y: 23.19 },
+    { X: 12.41, Y: 22.91 },
+    { X: 24.29, Y: 20.08 },
+    { X: 22.11, Y: 24.88 },
+    { X: 16.08, Y: 28.09 },
+    { X: 10.55, Y: 28.18 },
+    { X: 2.18, Y: 27.62 },
+    { X: 1.17, Y: 33.93 },
+    { X: 7.71, Y: 38.36 },
+    { X: 11.73, Y: 35.06 },
+    { X: 15.91, Y: 37.42 },
+    { X: 20.11, Y: 39.4 },
+    { X: 26.63, Y: 48.92 },
+    { X: 20.61, Y: 47.5 },
+    { X: 14.74, Y: 48.73 },
+    { X: 10.72, Y: 46.47 },
+    { X: 4.36, Y: 47.13 },
+    { X: 86.77, Y: 12.44 },
+    { X: 79.73, Y: 17.34 },
+    { X: 95.14, Y: 15.93 },
+    { X: 96.31, Y: 18.28 },
+    { X: 93.31, Y: 21.02 },
+    { X: 86.93, Y: 21.21 },
+    { X: 30.32, Y: 73.14 },
+    { X: 78.39, Y: 23.19 },
+    { X: 78.22, Y: 23.19 },
+    { X: 96.65, Y: 28.46 },
+    { X: 91.46, Y: 26.58 },
+    { X: 86.93, Y: 28.09 },
+    { X: 82.58, Y: 27.24 },
+    { X: 96.98, Y: 35.91 },
+    { X: 87.94, Y: 38.08 },
+    { X: 84.25, Y: 36.48 },
+    { X: 96.15, Y: 46.65 },
+    // { X: 89.78, Y: 48.44 }, // Too close to loot
+    // { X: 82.91, Y: 49.29 }, // Too close to loot
+    // { X: 77.55, Y: 46.65 }, // Too close to loot
+    // { X: 71.36, Y: 49.48 }, // Too close to loot
+];
+
 const UI_DURATION = 250;
+const CLOSE_WIN_DURATION = 1000;
 const HIT_DURATION = 20;
 
-const TimeCreated = new Date().toISOString().replace(/[^\dT]/g, "").substring(0,15);
-let next = 1000;
-const action = {
-    TimeCreated,
-    Name: "Start Attack",
-    Events: [],
-    LoopType: "TillLoopNumber",
-    LoopNumber: 1,
-    LoopTime: 0,
-    LoopInterval: 0,
-    Acceleration: "1.0",
-    PlayOnStart: false,
-    DonotShowWindowOnFinish: false,
-    RestartPlayer: false,
-    RestartPlayerAfterMinutes: 60,
-    ShortCut: "",
-    UserName: "",
-    MacroId: "",
-};
+let next;
+let action;
+
+function newAction (Name) {
+    const TimeCreated = new Date().toISOString().replace(/[^\dT]/g, "").substring(0,15);
+    next = 1000;
+    action = {
+        TimeCreated,
+        Name,
+        Events: [],
+        LoopType: "TillLoopNumber",
+        LoopNumber: 1,
+        LoopTime: 0,
+        LoopInterval: 0,
+        Acceleration: "1.0",
+        PlayOnStart: false,
+        DonotShowWindowOnFinish: false,
+        RestartPlayer: false,
+        RestartPlayerAfterMinutes: 60,
+        ShortCut: "",
+        UserName: "",
+        MacroId: "",
+    };
+}
 
 function addEvent ({ duration = 50, ...rest }) {
     action.Events.push({...rest, Timestamp: next})
@@ -102,38 +178,34 @@ function generateDrag ({ X: startX, Y: startY }, { X: endX, Y: endY }) {
     });
 }
 
+function clickContinue () {
+    generateOneClick({ X: 71.11, Y: 73.11, duration: HIT_DURATION});
+}
+
 function generateClickFairies() {
     // Fairies fly along Y: 22/23 from X:12 to X:98
     // Every fairy could possibly trigger a "Continue"/"View Ad" window
     // Assume user has paid version, click "continue" button between each fairy click attempt
     // Should take ~2 secs to go full distance
     // click every 1 X values, total of 86 clicks, or 43/s
-    const continueClickBase = { X: 71.1, Y: 73.1 };
     let Y = 22.5;
     for (let X = 13.1; X < 98; X += 1) {
         generateOneClick({X, Y, duration: HIT_DURATION});
-        generateOneClick({...continueClickBase, duration: HIT_DURATION});
+        clickContinue();
     }
-    generateOneClick(continueClickBase);
+    clickContinue();
 }
 
-function ensureClosed() {
-    // tapping 1, then 2 twice will ensure the bottom panel is closed
-    generateOneKey('1');
-    generateOneKey('2');
-    generateOneKey('2');
+function activateSkills(skills) {
+    const { activate } = allSkills;
+    const { Y } = activate;
+    skills.forEach(skill => {
+        generateOneClick({ X: activate[skill], Y });
+    });
 }
 
-function activateCheapSkills() {
-    // E is for Midas Touch, Y is for Shadow Clone
-    generateOneKey('E');
-    generateOneKey('Y');
-}
 function activateAllSkills() {
-    generateOneKey('W');
-    generateOneKey('E');
-    generateOneKey('T');
-    generateOneKey('Y');
+    activateSkills(['DS', 'HoM', 'WC', 'SC']);
 }
 
 function pageUp(depth = 1) {
@@ -144,29 +216,45 @@ function pageUp(depth = 1) {
 
 function pageDown(depth = 1) {
     for (let i = 0; i < depth; i++) {
-        generateDrag({X: 49.21, Y: 93.88}, {X: 49.21, Y: 16.27});
+        generateDrag({X: 49.21, Y: 93.88}, {X: 49.21, Y: 21.27});
     }
 }
 
-function enlargeWindow() {
-    // generateOneClick({X: 80.71, Y: 54.42 });
+function ensureEnlarged() {
+    // Take your time with this shit
+    generateOneKey('1', CLOSE_WIN_DURATION); // random window
+    generateOneKey('4', CLOSE_WIN_DURATION); // window we care about
+    pageUp();
+    generateOneClick({X: 80.71, Y: 54.42 }); // Either the full screen button or nothing important
+    generateOneKey('4', CLOSE_WIN_DURATION); // close
 }
 
-function shrinkWindow() {
-    // generateOneClick({X: 81.96, Y: 1.71 });
+function ensureHeroMaxBuy() {
+    generateOneKey('2', CLOSE_WIN_DURATION);
+    generateOneClick({ X: 84.31, Y: 8.43 });
+    generateOneClick({ X: 9.84, Y: 8.51 });
+    generateOneKey('2', CLOSE_WIN_DURATION);
+}
+
+// Only way I know how to do this is to wait for potential boss timer to expire
+function ensureNotWaitingForBoss() {
+    sleep(90000);
+    generateOneKey('B');
 }
 
 function tapNineButtons(reversed) {
+    // we actually tap 18 times, juuuust in case
+    // the screen is off a bit
     const X = 83.38;
     let start = 87.65;
-    let inc = 9;
+    let inc = 4.5;
     if (reversed) {
         start = 15.65;
         inc = -9;
     }
-    for ( let i = 0; i < 9; i++) {
+    for ( let i = 0; i < 18; i++) {
         const Y = +(start - (inc * i)).toFixed(2);
-        generateOneClick({ X, Y });
+        generateOneClick({ X, Y, duration: UI_DURATION / 2 });
     }
 }
 
@@ -174,123 +262,214 @@ function onePageOfHeroes(reversed) {
     generateOneKey('2');
     pageUp();
     tapNineButtons(reversed);
-    generateOneKey('2', 2 * UI_DURATION);
+    generateOneKey('2', CLOSE_WIN_DURATION);
 }
 
 function twoPagesOfHeroes(reversed) {
     generateOneKey('2');
-    enlargeWindow();
     pageUp(); // Help ensure we line up with the taps
     pageDown();
     tapNineButtons();
     pageUp();
     tapNineButtons();
-    generateOneKey('2', 2 * UI_DURATION);
+    generateOneKey('2', CLOSE_WIN_DURATION);
 }
 
-function tapFor(time) {
+// mode: manic, normal, safe
+// manic: Keep all skills up, tap all astral
+// normal: Keep cheap skills up, tap all astral
+// safe: Keep cheap skills up, skip unsafe astral
+function tapFor(time, mode, skills = ['HoM', 'SC']) {
+    mode === 'manic' && skills.push('DS', 'WC');
+    const daggerDuration = mode === 'manic' ? HIT_DURATION : 1000;
+
     const startWait = next;
     while ( next - startWait < time ) {
+        // Desired skills
+        activateSkills(skills);
         // Crew
         generateOneClick({ X: 36.59, Y: 49.63, duration: HIT_DURATION });
         // Pet
-        generateOneClick({ X: 58.84, Y: 48.44, duration: HIT_DURATION });
+        // generateOneClick({ X: 58.84, Y: 48.44, duration: HIT_DURATION });
+        generateOneClick({ X: 52.92, Y: 48.64, duration: HIT_DURATION });
+        // Astral Awakening
+        for (let i = 0; i < 2; i++) {
+            astralLocations.forEach(({ X, Y }) => {
+                if ( Y > 35 || mode !== 'safe' ) {
+                    generateOneClick({ X, Y, duration: HIT_DURATION });
+                }
+            });
+            // Twice just to be sure; the above taps go FAST
+            clickContinue();
+            clickContinue();
+        }
         // Daggers
-        generateOneClick({ X: 35.06, Y: 42.31, duration: 1000 });
-        generateOneClick({ X: 42.41, Y: 44.81, duration: 1000 });
-        generateOneClick({ X: 50.27, Y: 45.12, duration: 1000 });
-        generateOneClick({ X: 56.89, Y: 43.81, duration: 1000 });
-        generateOneClick({ X: 64.41, Y: 41.99, duration: 1000 });
-        // Re-activate the cheap skills if they're down
-        activateCheapSkills();
+        generateOneClick({ X: 34.66, Y: 40.13, duration: daggerDuration });
+        generateOneClick({ X: 42.08, Y: 40.13, duration: daggerDuration });
+        generateOneClick({ X: 49.64, Y: 40.77, duration: daggerDuration });
+        generateOneClick({ X: 57.35, Y: 39.89, duration: daggerDuration });
+        generateOneClick({ X: 64.48, Y: 39.09, duration: daggerDuration });
     }
 }
 
-// mode init will add 1 lvl to main skills, max will use "all" button
-function increaseSkills (mode = '') {
+// skills: see "allSkills" above
+function increaseSkills (max = false, skills = ['DS', 'HoM', 'WC', 'SC']) {
+    const { levelUp } = allSkills;
     generateOneKey('1');
-    enlargeWindow();
     pageUp();
     // Level up Sword Dude
     generateOneClick({ X: 84.11, Y: 16.06 });
 
-    generateOneClick({ X: 83.21, Y: 48.69 });
-    if(mode === 'max') generateOneClick({ X: 58.93, Y: 48.69 });
-
-    if(mode === 'init') generateOneClick({ X: 83.21, Y: 57.69 });
-
-    generateOneClick({ X: 83.21, Y: 75.69 });
-    if(mode === 'max') generateOneClick({ X: 58.93, Y: 75.69 });
-
-    if(mode === 'init') generateOneClick({ X: 83.21, Y: 84.69 });
-
-    generateOneKey('1');
+    skills.forEach(skill => {
+        const Y = levelUp[skill];
+        generateOneClick({ X: levelUp.oneX, Y });
+        if(max) {
+            // Need to handle if above canceled active skill
+            generateOneClick({ X: levelUp.oneX, Y });
+            generateOneClick({ X: levelUp.maxX, Y });
+        }
+    });
+    generateOneKey('1', CLOSE_WIN_DURATION);
 }
 
-function earlyLoop () {
-    activateCheapSkills();
+function manic60s () {
+    onePageOfHeroes();
+    tapFor(50000, 'manic');
+    sleep(2000); // visible loop marker
+}
+
+function normal60s (skills) {
+    onePageOfHeroes();
+    tapFor(50000, 'normal', skills);
+    sleep(2000); // visible loop marker
+}
+
+function safe60s (skills) {
+    onePageOfHeroes();
+    tapFor(50000, 'safe', skills);
+    sleep(2000); // visible loop marker
+}
+
+function splitLoop (skills) {
     generateClickFairies();
     activateAllSkills();
-    onePageOfHeroes();
-    tapFor(124000);
-    sleep(2000); // Padding
+    onePageOfHeroes(true);
+    tapFor(60000, 'normal', skills);
+    onePageOfHeroes(true);
+    tapFor(45000, 'safe', skills);
+}
+
+function midLoop () {
+    const skills = ['HoM', 'WC', 'SC'];
+    activateSkills(skills);
+    splitLoop(skills);
 }
 
 function lateLoop () {
-    activateCheapSkills();
-    generateClickFairies();
-    activateAllSkills();
-    onePageOfHeroes(true);
-    tapFor(60000);
-    onePageOfHeroes(true);
-    tapFor(45000);
+    const skills = ['HoM','SC'];
+    activateSkills(skills);
+    splitLoop(skills);
 }
 
 function prestige () {
-    generateOneKey('1');
-    generateOneClick({ X: 84.97, Y: 29.31, duration: 2 * UI_DURATION });
-    generateOneClick({ X: 49.91, Y: 87.21, duration: 2 * UI_DURATION  });
-    generateOneClick({ X: 69.41, Y: 71.51, duration: 2 * UI_DURATION  });
-    sleep(10000);
-    // Do something useless, just to mark the sleep time
-    generateOneKey('N');
+    generateOneKey('1', UI_DURATION * 2);
+    generateOneClick({ X: 83.24, Y: 28.89, duration: 2000});
+    generateOneClick({ X: 49.71, Y: 91.01, duration: 2000 }); // Event
+    generateOneClick({ X: 71.18, Y: 75.04, duration: 10000 }); // Event
+    // generateOneClick({ X: 49.71, Y: 81.46, duration: 2000 }); // Normal
+    // generateOneClick({ X: 67.94, Y: 67.72, duration: 10000 }); // Normal
+    // Might see a warning about equipment; clicky!
+    generateOneClick({ X: 50.88, Y: 65.81 });
 }
 
 function bookOfShadows () {
     generateOneKey('5');
-    for ( let i = 0; i < 5; i++) {
+    for ( let i = 0; i < 6; i++) {
         generateOneClick({ X: 84.26, Y: 28.71 });
     }
     generateOneKey('5');
 }
 
-function generateScript () {
-    ensureClosed();
+// const outputPath = process.argv[2];
+const outputPath = '/mnt/c/Users/Flare576/Documents/TT2_Macros';
 
-    // increaseSkills('init');
-    // The first loop is likely going to have a few straglers
-    // Tap 'em
-    earlyLoop();
-    twoPagesOfHeroes();
+let name = '1 Initialize Skills';
+newAction(name);
+ensureEnlarged();
+ensureHeroMaxBuy();
+increaseSkills();
+manic60s();
+twoPagesOfHeroes();
+writeIt(name);
 
-    // Loop 9 more times
-    for (let i = 0; i < 7; i++) earlyLoop();
-    increaseSkills('max');
+name = '2.1 Manic Loop 60s';
+newAction(name);
+manic60s();
+writeIt(name);
 
-    // There will be 1 more lateLoop cycle after this set of for's
-    const loopsPerCycle = 4;
-    const cycles = 2;
-    for (let i = 1; i < cycles; i++) {
-        for (let j = 0; j < loopsPerCycle; j++) lateLoop();
-        increaseSkills(); // My current boss timer, wait one out to ensure we failed
-        sleep(90000);
-        generateOneKey('B');
-    }
-    for (let j = 0; j < loopsPerCycle; j++) lateLoop();
-    prestige();
-    bookOfShadows();
+name = '2.2 Normal Loop 60s (HoM+SC)';
+newAction(name);
+normal60s(['HoM', 'SC']);
+writeIt(name);
+
+name = '2.3 Safe Loop 60s (HoM+SC)';
+newAction(name);
+safe60s(['HoM', 'SC']);
+writeIt(name);
+
+name = '2.4 Split Loop 120s (HoM+SC)';
+newAction(name);
+safe60s(['HoM', 'SC']);
+writeIt(name);
+
+name = '2.5 10s Pause';
+newAction(name);
+sleep(10000);
+writeIt(name);
+
+name = '3.1 Mid Transition [Max DS]';
+newAction(name);
+increaseSkills(true, ['DS']);
+writeIt(name);
+
+name = '3.2 Late Transition [Max WC]';
+newAction(name);
+increaseSkills(true, ['WC']);
+writeIt(name);
+
+name = '3.3 Prestige';
+newAction(name);
+prestige();
+writeIt(name);
+
+name = '3.4 BoS Max Upgrade';
+newAction(name);
+bookOfShadows();
+writeIt(name);
+
+name = '4.1 Mid Loop x3 with Leave-Join'
+newAction(name);
+for (let j = 0; j < 3; j++) midLoop();
+increaseSkills(false, ['DS']);
+sleep(5000); // Buffer
+writeIt(name);
+
+name = '4.2 Late Loop x3 with 90s pause'
+newAction(name);
+for (let j = 0; j < 3; j++) lateLoop();
+increaseSkills(false, ['DS', 'WC']);
+ensureNotWaitingForBoss();
+writeIt(name);
+
+name = '5. Test'
+newAction(name);
+// activateSkills(['SC']);
+safe60s();
+// writeIt(name);
+
+function writeIt(name) {
+    fs.writeFileSync(`${outputPath}/${name}.json`, JSON.stringify(action, null, 2));
 }
 
-generateScript();
-console.log(process);
-//console.log(JSON.stringify(action, null, 2));
+// generateScript();
+// console.log(JSON.stringify(action, null, 2));
